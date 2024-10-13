71f2567745983279ebb90a885aa9f2acd84be884
Nama Anda <email@example.com>
2024-10-13 23:38:13 +0700

    Tugas 3

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,7 +13,18 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('/user/profile', [UserController::class, 'profile']); 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/user/create', [UserController::class, 'create']);
+
+
+Route::post('/user/store', function (Request $request) {
+    
+    $nama = $request->input('nama');
+    $npm = $request->input('npm');
+    $kelas = $request->input('kelas');
+
+    
+    return "Nama: $nama, NPM: $npm, Kelas: $kelas";
+})->name('user.store');
